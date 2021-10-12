e9314cd6f0609ef019113218b41b6849d23696a8
Denny <denny1011133@luci.group>
2021-10-12 14:03:10 +0800

    feat router link with Link component

--- src/components/MyFavorites.js
+++ src/components/MyFavorites.js
@@ -5,20 +5,26 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 function MyFavorites({ myFavorites, handleDelete }) {
   return (
     <Table
-      style={{ marginLeft: "224.8px" }}
-      responsive
+      style={{
+        textAlign: "center",
+        marginLeft: "230px",
+        marginTop: "20px",
+        width: "80vw",
+      }}
       striped
       bordered
       hover
+      size="lg"
       variant="dark"
+      responsive
     >
       <thead>
         <tr>
-          <th>#</th>
-          <th>專輯名稱</th>
-          <th>演唱者</th>
-          <th>類型</th>
-          <th>移除</th>
+          <th style={{ width: "3%" }}>#</th>
+          <th style={{ width: "5%" }}>專輯名稱</th>
+          <th style={{ width: "5%" }}>演唱者</th>
+          <th style={{ width: "3%" }}>類型</th>
+          <th style={{ width: "3%" }}>移除</th>
         </tr>
       </thead>
       <tbody>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faColumns, faHeart, faMusic } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 function SideBar() {
   return (
     <div className="sidenav">
-      <a href="/" className="fw-bold">
-        <FontAwesomeIcon icon={faMusic} size="lg" />
-        {` Music`}
-      </a>
+      <Link className="fw-bold" to="/"> <FontAwesomeIcon icon={faMusic} size="lg" />{` Music`} </Link>
       <div className="d-flex flex-column justify-content-evenly align-items-baseline gap-3 mt-2">
-        <Button href="/" variant="black"></Button>
-        <Button href="/albums" variant="black" style={{ fontSize: "1rem" }}>
-          <FontAwesomeIcon icon={faColumns} /> 瀏覽
-        </Button>
-        <Button
-          href="/myFavorites"
-          variant="black"
-          style={{ fontSize: "1rem" }}
-        >
-          <FontAwesomeIcon icon={faHeart} /> 我的收藏
-        </Button>
+        <Link style={{ fontSize: "1rem" }} to="/albums"> <FontAwesomeIcon icon={faColumns} /> {` 瀏覽`} </Link>
+        <Link style={{ fontSize: "1rem" }} to="/myFavorites"> <FontAwesomeIcon icon={faHeart} />{` 我的收藏`} </Link>
       </div>
       <hr />
     </div>
