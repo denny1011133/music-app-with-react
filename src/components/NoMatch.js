3911b5c891afec86953c1a5a2451e7d47e28445f
Denny <denny1011133@luci.group>
2021-10-12 10:27:12 +0800

    Adjust NoMatch css

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -2,29 +2,29 @@ function NoMatch() {
   return (
     <div
       style={{
-        width: "50%",
-        height: "50%",
+        height: "85vh",
         display: "flex",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        margin: "100px auto",
+        marginLeft: "224.8px",
       }}
     >
       <div>
         <h1>此頁面不存在。</h1>
         <p>我們找不到你正在尋找的網頁。或許我們的常見問題或社群幫得上忙？</p>
-
         <a
-          href="#"
+          href="/"
           onClick={() => {
             window.history.back();
           }}
         >
-          返回
+          返回首頁
         </a>
       </div>
       <img
-        width="50%"
+        style={{ minWidth: "200px" }}
+        width="20%"
         src="https://www.scdn.co/i/404/record.svg"
         alt="CD"
       ></img>
